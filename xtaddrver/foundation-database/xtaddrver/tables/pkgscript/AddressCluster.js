212e346c33aae1a03a5536ea86c5f82ebcbfea0d
Gil Moskowitz <gilmoskowitz@acm.org>
2018-11-13 11:23:54 -0500

    update copyright/license notice

--- xtaddrver/foundation-database/xtaddrver/tables/pkgscript/AddressCluster.js
+++ xtaddrver/foundation-database/xtaddrver/tables/pkgscript/AddressCluster.js
@@ -1,12 +1,10 @@
 /*
   This file is part of the xtaddrver Package for xTuple ERP,
-  and is Copyright (c) 1999-2018 by OpenMFG LLC, d/b/a xTuple.  It
-  is licensed to you under the xTuple End-User License Agreement ("the
-  CPAL"), the full text of which is available at www.xtuple.com/EULA.
-  While the CPAL gives you access to source code and encourages your
-  involvement in the development process, this Package is not free
-  software.  By using this software, you agree to be bound by the
-  terms of the CPAL.
+  and is Copyright (c) 1999-2018 by OpenMFG LLC, d/b/a xTuple.
+  It is licensed to you under the Common Public Attribution License
+  version 1.0, the full text of which (including xTuple-specific Exhibits)
+  is available at www.xtuple.com/CPAL.  By using this software, you agree
+  to be bound by its terms.
 */
 
 (function () {
